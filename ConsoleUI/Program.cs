ebd6a7f0378d045d07fa52803ec17ea6ca08f9d6
Kutay Tistikk <kutaytistikk99@gmail.com>
2022-12-03 19:55:02 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,45 @@
+﻿using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICategoryDal categoryDal = new EfCategoryDal();
+            IProductDal productDal = new EfProductDal();
+            CategoryManager categoryManager = new CategoryManager(categoryDal);
+            ProductManager productManager = new ProductManager(productDal);
+
+            //foreach (var category in categoryManager.GetAll())
+            //{
+            //    Console.WriteLine(category.CategoryName);
+            //}
+
+            var result = productManager.GetProductDetails();
+
+            if (result.Success == true)
+            {
+
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,34 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Contex:DB tabloları ile proje classlarını bağlamak , ilişkilendirmek için kullanılacak
+
+    //DbContext Entity Framework ile gelen base bir sınıf
+
+    //öncellikle benim veritabanım şurada demem lazım
+
+    public class NorthwindContext : DbContext
+    {
+        //Proje ile Veritanbanı Bağlantısı Yapıldı
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true");
+        }
+
+        //Tablolar ile nesneler arası ilişkilendirilme işlemleri ,karşılık gelenleri belirleme yapılacak
+        //Benim projemdeki Product nesnesi ile veritabanındaki Products tablosunu bağla
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+
+
+    }
+}
