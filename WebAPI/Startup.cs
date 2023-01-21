908db5d30946a24940d72241a08128d047bb0903
Kutay Tıstık <kutaaytistik99@outlook.com>
2023-01-21 14:39:08 +0300

    21.01.2023

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -22,6 +22,7 @@ namespace WebAPI
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
