0aceb1eb0514d8805386d1a8ea43679998e126ba
Kutay Tıstık <kutaaytistik99@outlook.com>
2023-04-01 00:04:16 +0300

    01.04.2023

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
@@ -16,24 +18,69 @@ namespace Business.Concrete
         {
             _productDal = productDal;
         }
-        public List<Product> GetAll()
+
+        public IResult Add(Product product)
         {
-            return _productDal.GetAll();
+            //business codes
+
+            if (product.ProductName.Length < 2)
+            {
+                //magic strings
+                return new ErrorResult(Messages.ProductNameInvalid);
+            }
+            _productDal.Add(product);
+            return new SuccessResult(Messages.ProductAdded);
+
         }
 
-        public List<Product> GetAllByCategoryId(int id)
+        public IResult Delete(Product product)
         {
-            return _productDal.GetAll(p=>p.CategoryId==id);
+            _productDal.Delete(product);
+            return new SuccessResult();
         }
 
-        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        public IDataResult<List<Product>> GetAll()
         {
-            return _productDal.GetAll(p => p.UnitPrice <= max && p.UnitPrice >= min);
+            if (DateTime.Now.Hour==22)
+            {
+                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            }
+            var result=_productDal.GetAll();
+            return new SuccessDataResult<List<Product>>(result,Messages.ProductsListed);
         }
 
-        public List<ProductDetailDto> GetProductDetails()
+        public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
-            return _productDal.GetProductDetails();
+            var result=_productDal.GetAll(p => p.CategoryId == id);
+            return new SuccessDataResult<List<Product>>(result);
+        }
+
+        public IDataResult<Product> GetById(int productId)
+        {
+            var result=_productDal.Get(p => p.ProductId == productId);
+            return new SuccessDataResult<Product>(result);
+        }
+
+        public IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max)
+        {
+            var result=_productDal.GetAll(p => p.UnitPrice <= max && p.UnitPrice >= min);
+            return new SuccessDataResult<List<Product>>(result);
+        }
+
+        public IDataResult<List<ProductDetailDto>> GetProductDetails()
+        {
+            if (DateTime.Now.Hour == 00)
+            {
+                return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
+            }
+            var result=_productDal.GetProductDetails();
+            return new SuccessDataResult<List<ProductDetailDto>>(result);
+        }
+
+        public IResult Update(Product product)
+        {
+            _productDal.Update(product);
+            return new SuccessResult(Messages.ProductUpdated);
         }
     }
 }
---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        //Product
+        public static string ProductAdded = "Product Added Successfully";
+        public static string ProductNameInvalid = "Product Name Must Be A Minimum Of Two Characters";
+        public static string MaintenanceTime = "Maintenance Time";
+        public static string ProductsListed = "Products Listed Successfully";
+        public static string ProductUpdated = "Product Updated Successfully";
+    }
+}
